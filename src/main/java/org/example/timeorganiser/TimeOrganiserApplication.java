e23246d63b295838707f07d25bf1783867811b0c
KoaliM <mihaelahristova071@gmail.com>
2026-04-23 00:08:30 +0300

    first commit

---
+++ src/main/java/org/example/timeorganiser/TimeOrganiserApplication.java
@@ -0,0 +1,13 @@
+package org.example.timeorganiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeOrganiserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimeOrganiserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/timeorganiser/TimeOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.timeorganiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeOrganiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
